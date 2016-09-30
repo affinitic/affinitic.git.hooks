7c575dc3f0087036c699b026f8cd17e359958000
Sébastien Sirtoli <sebastien.sirtoli@affinitic.be>
2016-09-30 16:55:17 +0200

    Add setup.py file
    This refs affinitic #6029

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+version = '0.1'
+
+long_description = (
+    open('README.txt').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+setup(name='affinitic.git.hooks',
+      version=version,
+      description="Affinitic git hooks",
+      long_description=long_description,
+      classifiers=["Programming Language :: Python"],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://svn.plone.org/svn/collective/',
+      license='gpl',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['git'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools'])
